a26e6b442549b0bb355fdb3d5c17ca0b9e92c439
vcaster <ainaniran@yahoo.com>
2021-05-03 15:08:09 -0400

    estimate, profit,sales API

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 const db = mysql.createConnection({
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
+    port     : process.env.DB_PORT,
     password : process.env.DB_PASS,
     database : process.env.DB_DB
 });
@@ -85,6 +86,53 @@ app.post('/api/employeeupdate', (req, res) => {
     });
 });
 
-app.listen('3000', () => {
-    console.log('Server started on port 3000');
+/*+++++++++++++++++++++++++++++++++++++++
+            Estimate
++++++++++++++++++++++++++++++++++++++++++*/
+
+app.post('/api/estimate', (req, res) => {
+    let sql = `SELECT t1.ingreName,consumed,supplied,(consumed+supplied)/2 AS Estimate FROM
+    (SELECT C.ingreID,ingreName,SUM(quantity) AS consumed FROM CONSUMELOG AS C JOIN INGREDIENTS AS I ON C.ingreID = I.ingreID WHERE cdate BETWEEN '${req.body.startdate}' AND '${req.body.enddate}' GROUP BY I.ingreID) t1
+    INNER JOIN
+    (SELECT S.ingreID,ingreName,SUM(quantity) AS supplied FROM SUPPLYLOG AS S JOIN INGREDIENTS AS I ON S.ingreID = I.ingreID WHERE sdate BETWEEN '${req.body.startdate}' AND '${req.body.enddate}' GROUP BY I.ingreID) t2
+    ON t1.ingreID=t2.ingreID`;
+    console.log(req.body)
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log("Success!");
+        res.send(results);
+    });
+});
+
+/*+++++++++++++++++++++++++++++++++++++++
+            Profit
++++++++++++++++++++++++++++++++++++++++++*/
+
+app.post('/api/profit', (req, res) => {
+    let sql = `SELECT pizzaName,SUM((cost*quantity)-(price*quantity)) AS profit FROM SALES AS S JOIN PIZZA AS P ON S.pizzaID=P.pizzaID WHERE saledate BETWEEN '${req.body.startdate}' AND '${req.body.enddate}' GROUP BY P.pizzaID`;
+    console.log(req.body)
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log("Success!");
+        res.send(results);
+    });
+});
+
+/*+++++++++++++++++++++++++++++++++++++++
+            Sales
++++++++++++++++++++++++++++++++++++++++++*/
+
+app.post('/api/sales', (req, res) => {
+    let sql = `SELECT pizzaName,SUM(quantity) AS sold FROM SALES AS S JOIN PIZZA AS P ON S.pizzaID=P.pizzaID WHERE saledate BETWEEN '${req.body.startdate}' AND '${req.body.enddate}' GROUP BY P.pizzaID`;
+    console.log(req.body)
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log("Success!");
+        res.send(results);
+    });
+});
+
+
+app.listen('3001', () => {
+    console.log('Server started on port 3001');
 });
